d379bb40f755b1133be2c38bd27f5403f82d6c09
arthurpontespiazzaroli@gmail.com <arthurpontespiazzaroli@gmail.com>
2022-11-26 20:25:53 -0300

    Criacao rotas,baixando dependencias,criacao pastas

---
+++ lembretes/src/pages/CadastrarLembrete/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+export default function CadastrarLembrete(){
+    return(
+        <View>
+            <Text>LembraRápido</Text>
+        </View>
+    );
+}
---
+++ lembretes/src/pages/EditarLembrete/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View,Text } from "react-native";
+
+export default function EditarLembrete(){
+    return(
+        <View>
+            <Text>LembraRápido</Text>
+        </View>
+    );
+}
