9e8d19131c17c6e37dae023253c3ea8951e65b4a
osbarros <ceobarros@outlook.com>
2022-11-29 18:33:40 -0300

    fix: instalação correta das dependências do react navigation

--- lembretes/App.js
+++ lembretes/App.js
@@ -3,30 +3,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import Cadastro from './src/pages/Cadastro';
 import Login from './src/pages/Login';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Routes from "./routes"
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Cadastro'>
-
-        <Stack.Screen
-          name="Cadastro"
-          component={Cadastro}
-          options={{
-            title: "Cadastro"
-          }}
-        />
-
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            title: "Login"
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Routes/>
   )
 }
